d49b1e5ba3c474f5bad9cb629e915bcc2320a797
wangxiaoyuwf <wangxiaoyuwf@126.com>
2022-03-21 21:41:26 +0800

    add some comments

--- do_format.py
+++ do_format.py
@@ -1,8 +1,11 @@
 import sys, getopt
 
 
+# 定义保存运行结果的路径
 FILES_PATH = "do_format_result/"
 
+
+# 逐行读取，并把读到的整行交给replace_some_character方法
 def read_lines(path):
     f = open(path)
     while 1:
@@ -12,11 +15,17 @@ def read_lines(path):
         if not line:
             break
         # print(len(line))
+        # 替换一些字符，并且保存成代码文件
         replace_some_character(line)
     f.close()
 
 
+# 替换换行，tab, \"等特殊字符，并且保存在文件中
+# var BlitScreenPS = "#if defined 为例。去掉后缀PS， BlitScreen是文件名，如果是PS结尾的，后缀为.fs
+# VS结尾的，后缀名为.vs,其他的，文件名不变，后缀为.glsl
 def replace_some_character(str):
+    # 字符串经过处理后，拿到文件名
+    # 文件名为.fs .vs 或 .glsl
     file_name = str.split(" ")[1]
     if file_name.endswith("PS"):
         file_name = FILES_PATH + file_name[:-2] + ".fs"
@@ -25,10 +34,15 @@ def replace_some_character(str):
     else:
         file_name = FILES_PATH + file_name + ".glsl"
     print("file name: ", file_name)
+    # 把最后的三个字符（";还有换行符）去掉
     str = str[:-3]
+    # 把第一个引号前面的内容去掉
     str = str.split('= \"')[1]
+    # \t 替换成 tab
     str = str.replace("\\t", "\t")
+    # \r\n 替换成换行
     str = str.replace("\\r\\n", "\r\n")
+    # \" 替换成"
     str = str.replace('\\"', '\"')
     # print(str)
     # 把内容写入文件
